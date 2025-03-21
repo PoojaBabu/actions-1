0731b194ea96f6569026d2eff6450e0e396fe66c
Pooja <s.pooja1908@gmail.com>
2025-03-21 16:44:46 -0700

    running a script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,7 @@
+echo "My first Github action job"
+ls -lrtha
+cat README.md
+sudo apt-get install cow
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+cat dragon.txt
+ls -lrtha
