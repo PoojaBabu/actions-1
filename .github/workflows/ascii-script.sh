9e53bbf5c252946049a8074b706431d3b3dc3e76
Pooja <s.pooja1908@gmail.com>
2025-03-21 17:11:51 -0700

    take 4

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt 
+grep -i "DRAGON" dragon.txt 
 cat dragon.txt
 ls -lrtha
